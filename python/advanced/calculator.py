e0a1834c0d3edb28d4f8d781f0310f8f86b7bc70
Mike <mykhail.nakonechnyi@gmail.com>
2023-07-06 21:33:06 +0300

    add exception for HW 1 - task: calculator

--- python/advanced/calculator.py
+++ python/advanced/calculator.py
@@ -13,6 +13,11 @@ class Calculator:
         }
 
     def hello(self):
+        """_summary_
+
+        Returns:
+            _type_: _description_
+        """
         return self.description
 
     def add(self, number1, number2):
@@ -35,27 +40,30 @@ class Calculator:
 
     def run_calculator(self):
         """Run the calculator program."""
+        try:
+            # Prompt the user to enter two numbers
+            num1 = float(input("Please enter the first number: "))
+            num2 = float(input("Please enter the second number: "))
 
-        # Prompt the user to enter two numbers
-        num1 = float(input("Please enter the first number: "))
-        num2 = float(input("Please enter the second number: "))
+            # Prompt the user to select an operation
 
-        # Prompt the user to select an operation
+            print("\nPlease select an operation:")
+            print("1. Addition")
+            print("2. Subtraction")
+            print("3. Multiplication")
+            print("4. Division")
 
-        print("\nPlease select an operation:")
-        print("1. Addition")
-        print("2. Subtraction")
-        print("3. Multiplication")
-        print("4. Division")
+            operation = int(input("Enter your choice (1-4): "))
 
-        operation = int(input("Enter your choice (1-4): "))
-
-        # Perform the corresponding calculation based on the selected operation
-        if operation in self.operations:
-            try:
-                result = self.operations[operation](num1, num2)
-                print("\nThe result is:", result)
-            except ZeroDivisionError as e:
-                print("Error:", str(e))
-        else:
-            print("Invalid choice. Please select a valid operation (1-4).")
+            # Perform the corresponding calculation based on the selected
+            # operation
+            if operation in self.operations:
+                try:
+                    result = self.operations[operation](num1, num2)
+                    print("\nThe result is:", result)
+                except ZeroDivisionError as zeroexc:
+                    print("Error:", str(zeroexc))
+            else:
+                print("Invalid choice. Please select a valid operation (1-4).")
+        except ValueError:
+            print("Invalid input. Please enter valid numbers.")
